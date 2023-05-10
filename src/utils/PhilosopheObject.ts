cc357fcae20411d5d4c36d450f2340261bfeee83
johan <johancampion13@gmail.com>
2023-05-10 14:39:44 +0200

    update with die possibility

--- src/utils/PhilosopheObject.ts
+++ src/utils/PhilosopheObject.ts
@@ -1,20 +1,39 @@
 import {Semaphore} from "./Semaphore";
+import {interval, timer} from "rxjs";
 
 export class PhilosopheObject {
 
-  state: 'pense' | 'mange' = 'pense' ;
+  state: 'pense' | 'mange' | 'affame' | 'mort' = 'pense' ;
 
-  tempPourPenser = 3000;
-  tempPourManger = 10000;
 
   fourchetteGauche: Semaphore;
   fourchetteDroite: Semaphore;
   nom: String;
 
-  pause = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
   min = 3000;
   max = 6000;
+  tempsBeforeDie = 60;
+  _tempsBeforeDie: number = this.tempsBeforeDie;
+  interval: any;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this._tempsBeforeDie > 0) {
+        this._tempsBeforeDie--;
+        if (this._tempsBeforeDie == 0) {
+          this.state = 'mort';
+        }
+      } else {
+        this._tempsBeforeDie = this.tempsBeforeDie;
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+    this._tempsBeforeDie = this.tempsBeforeDie;
+  }
+
 
 
 
@@ -25,24 +44,31 @@ export class PhilosopheObject {
   }
 
   async penser() {
-    this.state = "pense";
-    console.log(`${this.nom} est entrain de penser`);
-    await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (this.max - this.min + 1)) + this.min));
-    await this.eat();
+    if (this.state !== 'mort') {
+      this.state = "pense";
+      console.log(`${this.nom} est entrain de penser`);
+      await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (this.max - this.min + 1)) + this.min));
+      this.state = "affame";
+      this.startTimer()
+      await this.eat();
+    }
   }
 
   async eat() {
-    console.log(`${this.nom} is picking up forks`);
-    await Promise.all([this.fourchetteGauche.acquire(), this.fourchetteDroite.acquire()]);
-    console.log(`${this.nom} is eating`);
-    this.state = "mange"
-    await new Promise(resolve => setTimeout(resolve, 3000)); // philosopher eats for 10 seconds
-    console.log(`${this.nom} is putting down forks`);
-    await Promise.all([this.fourchetteGauche.release(), this.fourchetteDroite.release()]);
+    if (this.state !== 'mort') {
+      console.log(`${this.nom} is picking up forks`);
+      await Promise.all([this.fourchetteGauche.acquire(), this.fourchetteDroite.acquire()]);
+      console.log(`${this.nom} is eating`);
+      this.state = "mange"
+      this.pauseTimer()
+      await new Promise(resolve => setTimeout(resolve, 30000)); // philosopher eats for 10 seconds
+      console.log(`${this.nom} is putting down forks`);
+      await Promise.all([this.fourchetteGauche.release(), this.fourchetteDroite.release()]);
+    }
   }
 
   async dine() {
-    while (true) {
+    while (true && this.state !== 'mort') {
       await this.penser();
     }
   }
